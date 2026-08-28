30916198959a0b740faa50ba70acb75670483d50
svitanoksm <svitanoksm@gmail.com>
2026-08-28 15:58:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ st.set_page_config(
 pg = st.navigation([
     st.Page("pages/00_Irrigation.py", title="Зрошення", icon="💧"),
     st.Page("pages/01_Field_Parameters.py", title="Параметри полів", icon="⚙️"),
+    st.Page("pages/02_Irrigation_Control.py", title="Керування приладами", icon="🎛️"),
 ])
 
 pg.run()
