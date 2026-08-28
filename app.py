7418155c6bcbaac0a784577db66362dbe3c22532
svitanoksm <svitanoksm@gmail.com>
2026-08-28 19:54:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,34 @@ st.set_page_config(
     layout="wide",
 )
 
-# Налаштування красивого меню з кириличними назвами та іконками
+# ============================================================
+# МЕНЮ
+# ============================================================
+
 pg = st.navigation([
-    st.Page("pages/00_Irrigation.py", title="Зрошення", icon="💧"),
-    st.Page("pages/01_Field_Parameters.py", title="Параметри полів", icon="⚙️"),
-    st.Page("pages/02_Irrigation_Control.py", title="Керування приладами", icon="🎛️"),
+    st.Page(
+        "pages/00_Irrigation.py",
+        title="Зрошення",
+        icon="💧",
+    ),
+
+    st.Page(
+        "pages/01_Field_Parameters.py",
+        title="Параметри полів",
+        icon="⚙️",
+    ),
+
+    st.Page(
+        "pages/02_Irrigation_Control.py",
+        title="Керування приладами",
+        icon="🎛️",
+    ),
+
+    st.Page(
+        "pages/03_Tuya_Diagnostic.py",
+        title="Діагностика Tuya",
+        icon="🔎",
+    ),
 ])
 
 pg.run()
